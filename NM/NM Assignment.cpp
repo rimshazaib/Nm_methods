d3ce8a78cfd2359598a464030627fdc684c15b36
Your Name <you@example.com>
2022-07-23 05:49:28 -0700

    first committ

---
+++ NM/NM Assignment.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define EPSILON 0.001
+using namespace std;
+
+int main()
+{
+	double a;
+	cout<<"Welcome to the Solution of System of Non-Linear Equations"<<endl;
+	cout<<"Enter your Problem"<<endl;
+	cin>>a;
+	
+}
+
